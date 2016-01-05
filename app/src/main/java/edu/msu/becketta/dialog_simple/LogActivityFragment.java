1b11364e4d3179932b8708d76aa941ff79ea7a02
Aaron Beckett <becketta@msu.edu>
2016-01-05 16:24:13 -0500

    Add floating action button for annotation toggle.

--- app/src/main/java/edu/msu/becketta/dialog_simple/LogActivityFragment.java
+++ app/src/main/java/edu/msu/becketta/dialog_simple/LogActivityFragment.java
@@ -1,7 +1,9 @@
 package edu.msu.becketta.dialog_simple;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,5 +25,18 @@ public class LogActivityFragment extends Fragment {
     public void setImagePath(String path) {
         LogView logView = (LogView) getView().findViewById(R.id.logView);
         logView.setImagePath(path);
+
+        FloatingActionButton annotFAB = (FloatingActionButton) getView().findViewById(R.id.annotateFAB);
+        annotFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleAnnotate();
+            }
+        });
+        annotFAB.setVisibility(View.VISIBLE);
+    }
+
+    public void toggleAnnotate() {
+        Log.i("FAB", "Pressed");
     }
 }
